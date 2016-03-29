cf6ba8140541ab6156805f32b2ab58a50db1bd38
Alexander Gabriel <alex@gabriel-software.ch>
2016-03-30 01:20:54 +0200

    comment better

--- app/actions/geschaefte.js
+++ app/actions/geschaefte.js
@@ -1,5 +1,8 @@
 'use strict'
 
+// TODO: this line produces error
+// see: https://github.com/mapbox/node-sqlite3/issues/621
+// see: https://github.com/mapbox/node-pre-gyp/pull/187
 const getGeschaefte = require('../src/getGeschaefte.js')
 
 export const GESCHAEFTE_HOLEN = 'GESCHAEFTE_HOLEN'
