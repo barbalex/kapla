aee046271bf2eac2a5bd6b2d67f11211ec6de17b
Alexander Gabriel <alex@gabriel-software.ch>
2016-03-30 11:30:19 +0200

    get sqlite3 to finally work

--- app/actions/geschaefte.js
+++ app/actions/geschaefte.js
@@ -3,6 +3,7 @@
 // TODO: this line produces error
 // see: https://github.com/mapbox/node-sqlite3/issues/621
 // see: https://github.com/mapbox/node-pre-gyp/pull/187
+// solve with: http://verysimple.com/2015/05/30/using-node_sqlite3-with-electron/
 const getGeschaefte = require('../src/getGeschaefte.js')
 
 export const GESCHAEFTE_HOLEN = 'GESCHAEFTE_HOLEN'
@@ -16,10 +17,10 @@ function ersetze (geschaefte) {
 
 export function hole () {
   return dispatch => {
-    /*getGeschaefte()
+    getGeschaefte()
       .then((geschaefte) => {
         dispatch(ersetze(geschaefte))
       })
-      .catch((error) => console.log(error))*/
+      .catch((error) => console.log(error))
   }
 }
--- app/src/getGeschaefte.js
+++ app/src/getGeschaefte.js
@@ -1,7 +1,8 @@
 'use strict'
 
 const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database(':kapla:')
+// const db = new sqlite3.Database(':kapla:')
+const db = new sqlite3.Database('kapla.db')
 
 module.exports = function () {
   const sql = `
