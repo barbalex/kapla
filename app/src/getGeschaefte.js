48e2989b4f4fedf7288def8054d34dd3cc01dc04
Alexander Gabriel <alex@gabriel-software.ch>
2016-03-30 14:13:39 +0200

    optimize code

--- app/src/getGeschaefte.js
+++ app/src/getGeschaefte.js
@@ -6,14 +6,13 @@ const db = new sqlite3.Database('kapla.db')
 module.exports = function () {
   return new Promise((resolve, reject) => {
     const sql = `
-    SELECT
-      *
-    FROM
-      geschaefte
-    `
+      SELECT
+        *
+      FROM
+        geschaefte`
+
     db.all(sql, (error, result) => {
       if (error) reject(error)
-      console.log('getGeschaefte.js, result', result)
       resolve(result)
     })
   })
