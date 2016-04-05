15e183a5ee0f53ba2fb7986159beda347c61a7c0
Alexander Gabriel <alex@gabriel-software.ch>
2016-04-05 22:47:06 +0200

    remove counter

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux'
 import { routerReducer as routing } from 'react-router-redux'
-import counter from './counter'
 import geschaefte from './geschaefte'
 import user from './user'
 import app from './app'
 
 const rootReducer = combineReducers({
-  counter,
   geschaefte,
   user,
   routing,
--- app/routes.js
+++ app/routes.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import App from './containers/App'
-import CounterPage from './containers/CounterPage'
 import Geschaeft from './containers/Geschaeft'
 import Geschaefte from './containers/Geschaefte'
 
 export default (
-  <Route path='/' component={App}>
+<Route path='/' component={App}>
     <IndexRoute component={Geschaefte} />
-    <Route path='/counter' component={CounterPage} />
     <Route path='/geschaeft' component={Geschaeft} />
     <Route path='/geschaefte' component={Geschaefte} />
   </Route>
