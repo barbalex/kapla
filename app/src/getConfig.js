d77edbfa43014690bd8360263cd4a3d46ba05264
Alexander Gabriel <alex@gabriel-software.ch>
2016-04-15 08:49:27 +0200

    comment better

--- app/src/getConfig.js
+++ app/src/getConfig.js
@@ -2,8 +2,11 @@
 
 // see: http://www.mylifeforthecode.com/saving-and-restoring-window-state-in-electron/
 
+// this module will be called from main or inside app
+// so get app from electron or from electron.remote
 const electron = require('electron')
 const app = electron.app ? electron.app : electron.remote.app
+
 const fs = require('fs')
 const path = require('path')
 const dataFilePath = path.join(app.getPath('userData'), 'kaplaConfig.json')
