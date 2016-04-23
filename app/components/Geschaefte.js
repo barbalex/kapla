1ef319fefe86e7f8f661a0443e6131e2fa6864fc
Alexander Gabriel <alex@gabriel-software.ch>
2016-04-23 21:52:03 +0200

    optimize geschaefte

--- app/components/Geschaefte.js
+++ app/components/Geschaefte.js
@@ -2,6 +2,7 @@
 
 import React, { Component, PropTypes } from 'react'
 import ReactList from 'react-list'
+import moment from 'moment'
 import styles from './Geschaefte.css'
 
 class Geschaefte extends Component {
@@ -18,12 +19,37 @@ class Geschaefte extends Component {
     geschaeftToggleActivated(idGeschaeft)
   }
 
+  dauerBisFristMitarbeiter = (geschaeft) => {
+    if (!geschaeft.fristMitarbeiter) return null
+    const now = moment()
+    const end = moment(geschaeft.fristMitarbeiter, 'DD.MM.YYYY')
+    const duration = moment.duration(end.diff(now))
+    const days = duration.asDays()
+    return days ? Math.ceil(days) : ''
+  }
+
+  statusFristInText = (dauerBisFristMitarbeiter) => {
+    if (dauerBisFristMitarbeiter < 0) return 'Überfällig'
+    if (dauerBisFristMitarbeiter === 0) return 'Heute'
+    if (dauerBisFristMitarbeiter === 1) return `In ${dauerBisFristMitarbeiter} Tag`
+    return `In ${dauerBisFristMitarbeiter} Tagen`
+  }
+
+  statusFristInStyle = (statusFristInText) => {
+    if (statusFristInText === 'Überfällig') return styles.fieldFristInUeberfaellig
+    if (statusFristInText === 'Heute') return styles.fieldFristInHeute
+    return null
+  }
+
   renderItem(index, key) {
     const { geschaefte, geschaefteGefilterteIds, activeId } = this.props
     const isActive = activeId && activeId === geschaefteGefilterteIds[index]
     const trClassName = isActive ? [styles.tableBodyRow, styles.active].join(' ') : styles.tableBodyRow
     const geschaeft = geschaefte.find((g) => g.idGeschaeft === geschaefteGefilterteIds[index])
     const fristMitarbeiter = geschaeft.fristMitarbeiter ? `Frist: ${geschaeft.fristMitarbeiter}` : ''
+    const dauerBisFristMitarbeiter = this.dauerBisFristMitarbeiter(geschaeft)
+    const statusFristInText = this.statusFristInText(dauerBisFristMitarbeiter)
+    const statusFristIn = geschaeft.fristMitarbeiter ? statusFristInText : null
 
     return (
       <div
@@ -31,12 +57,12 @@ class Geschaefte extends Component {
         className={trClassName}
         onClick={this.onClickGeschaeft.bind(this, geschaeft.idGeschaeft)}
       >
-        <div className={[styles.columnIdGeschaeft, styles.tableBodyCell].join(' ')}>
+        <div className={styles.columnIdGeschaeft}>
           <div>
             {geschaeft.idGeschaeft}
           </div>
         </div>
-        <div className={[styles.columnGegenstand, styles.tableBodyCell].join(' ')}>
+        <div className={styles.columnGegenstand}>
           <div className={styles.fieldGegenstand}>
             {geschaeft.gegenstand}
           </div>
@@ -44,18 +70,18 @@ class Geschaefte extends Component {
             {geschaeft.details}
           </div>
         </div>
-        <div className={[styles.columnStatus, styles.tableBodyCell].join(' ')}>
+        <div className={styles.columnStatus}>
           <div>
             {geschaeft.status}
           </div>
           <div>
             {fristMitarbeiter}
           </div>
-          <div>
-            {geschaeft.faelligkeitText}
+          <div className={this.statusFristInStyle(statusFristInText)}>
+            {statusFristIn}
           </div>
         </div>
-        <div className={[styles.columnKontaktIntern, styles.tableBodyCell].join(' ')}>
+        <div className={styles.columnKontaktIntern}>
           <div>
             {geschaeft.idKontaktIntern}
           </div>
