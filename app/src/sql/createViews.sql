553087b5f51ee82df05b95c133660d27dd2b6551
Alexander Gabriel <alex@gabriel-software.ch>
2016-04-25 22:51:06 +0200

    add kontakte intern to v_geschaefte

--- app/src/sql/createViews.sql
+++ app/src/sql/createViews.sql
@@ -12,6 +12,20 @@ AS
       ON geschaefteKontakteExtern.idKontakt = externe.id
   GROUP BY geschaefteKontakteExtern.idGeschaeft;
 
+DROP VIEW IF EXISTS v_geschaefte_kontakteIntern;
+CREATE VIEW
+  v_geschaefte_kontakteIntern
+AS
+  SELECT
+    geschaefteKontakteIntern.idGeschaeft AS idGeschaeft,
+    GROUP_CONCAT(interne.vorname || ' ' || interne.name) AS kontaktInternVornameName
+  FROM
+    interne
+    INNER JOIN
+      geschaefteKontakteIntern
+      ON geschaefteKontakteIntern.idKontakt = interne.id
+  GROUP BY geschaefteKontakteIntern.idGeschaeft;
+
 DROP VIEW IF EXISTS v_geschaefte;
 CREATE VIEW
   v_geschaefte
@@ -20,12 +34,16 @@ AS
     geschaefte.*,
     interne.vorname || ' ' || interne.name AS verantwortlichVornameName,
     interne.itKonto,
+    v_geschaefte_kontakteIntern.kontaktInternVornameName,
     v_geschaefte_kontakteExtern.kontaktExternVornameName
   FROM
     geschaefte
     LEFT JOIN
       interne
       ON geschaefte.verantwortlich = interne.kurzzeichen
+    LEFT JOIN
+      v_geschaefte_kontakteIntern
+      ON geschaefte.idGeschaeft = v_geschaefte_kontakteIntern.idGeschaeft
     LEFT JOIN
       v_geschaefte_kontakteExtern
       ON geschaefte.idGeschaeft = v_geschaefte_kontakteExtern.idGeschaeft
