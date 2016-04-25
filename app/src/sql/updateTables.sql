ebd8f84c1cf9059f8eb3db804f07904befc75952
Alexander Gabriel <alex@gabriel-software.ch>
2016-04-25 23:08:36 +0200

    reduce geschaefte table

--- app/src/sql/updateTables.sql
+++ app/src/sql/updateTables.sql
@@ -4,6 +4,8 @@
 -- remove old
 -- rename new
 
+
+-- interne
 insert into interne2 (id, abteilung, buero, eMail, itKonto, kurzzeichen, name, telefon, titel, vorname)
 select id, abteilung, buero, eMail, itKonto, kurzzeichen, name, telefon, titel, vorname
 from interne
@@ -13,5 +15,12 @@ drop table interne
 alter table interne2 rename to interne
 
 
+-- geschaefte
+INSERT INTO geschaefte2 (aktennummer, aktenstandort, benutzer, datumAusgangAwel, datumEingangAwel, details, entscheidAwelJahr, entscheidAwelNr, entscheidBdvJahr, entscheidBdvNr, entscheidKrJahr, entscheidKrNr, entscheidRrbJahr, entscheidRrbNr, fristAbteilung, fristAmtschef, fristAwel, fristDirektion, fristMitarbeiter, gegenstand, gekoNr, geschaeftsart, idGeschaeft,  idKontaktExtern_readonly, idKontaktIntern_readonly, idVorgeschaeft, mutationsdatum, mutationsperson, naechsterSchritt, ort, parlVorstossStufe, parlVorstossTyp, rechtsmittelerledigung, status, verantwortlich, vermerk, zustaendigeDirektion)
+SELECT aktennummer, aktenstandort, benutzer, datumAusgangAwel, datumEingangAwel, details, entscheidAwelJahr, entscheidAwelNr, entscheidBdvJahr, entscheidBdvNr, entscheidKrJahr, entscheidKrNr, entscheidRrbJahr, entscheidRrbNr, fristAbteilung, fristAmtschef, fristAwel, fristDirektion, fristMitarbeiter, gegenstand, gekoNr, geschaeftsart, idGeschaeft,  idKontaktExtern, idKontaktIntern, idVorgeschaeft, mutationsdatum, mutationsperson, naechsterSchritt, ort, parlVorstossStufe, parlVorstossTyp, rechtsmittelerledigung, status, verantwortlich, vermerk, zustaendigeDirektion
+FROM geschaefte
 
-id, abteilung, buero, eMail, itKonto, kurzzeichen, name, telefon, titel, vorname
+drop table geschaefte
+
+alter table geschaefte2 rename to geschaefte
+
