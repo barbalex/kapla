5abf507646d7857d5f0e43f329959cf82731d0e7
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-13 23:34:18 +0200

    format first row in xlsx

--- app/src/writeExport.js
+++ app/src/writeExport.js
@@ -18,6 +18,23 @@ process.on('message', (dataArray) => {
   const workbook = new Excel.Workbook()
   const worksheet = workbook.addWorksheet('Geschäfte')
   worksheet.addRows(dataArray)
+  worksheet.getRow(1).fill = {
+    type: 'gradient',
+    gradient: 'angle',
+    degree: 0,
+    stops: [
+      { position: 0, color: { argb: 'FFD3D3D3' } },
+      { position: 1, color: { argb: 'FFD3D3D3' } }
+    ]
+  }
+  worksheet.getRow(1).font = {
+    bold: true
+  }
+  worksheet.getRow(1).border = {
+    bottom: {
+      style: 'thin'
+    }
+  }
   workbook.xlsx.writeFile(path)
     .then(() => process.send({ success: true }))
     .catch((err) => process.send({ error: err }))
