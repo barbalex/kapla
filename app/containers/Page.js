11d2259a4cf0f719e96330985fdf95be68201cf9
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-02 18:28:18 +0200

    optimize code

--- app/containers/Page.js
+++ app/containers/Page.js
@@ -7,12 +7,23 @@ import * as GeschaefteActions from '../actions/geschaefte'
 import * as PagesActions from '../actions/pages'
 import * as AppActions from '../actions/app'
 
-const actions = Object.assign(AppActions, GeschaefteActions, PagesActions)
+const actions = Object.assign(
+  AppActions,
+  GeschaefteActions,
+  PagesActions
+)
 
 function mapStateToProps(state, props) {
   const { pages } = state
   const { pageIndex } = props
-  const { remainingGeschaefte, activePageIndex, title, queryTitle, building, reportType } = pages
+  const {
+    remainingGeschaefte,
+    activePageIndex,
+    title,
+    queryTitle,
+    building,
+    reportType
+  } = pages
   const { geschaefteGefilterteIds } = state.geschaefte
   const myGeschaefte = pages.pages[pageIndex].geschaefte
 
