12b4d01279aa7cb2a14dcdc581a589886566fc27
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-21 10:31:42 +0200

    convert to function

--- app/components/AreaPersonen.js
+++ app/components/AreaPersonen.js
@@ -7,108 +7,102 @@ import styles from './AreaPersonen.css'
 import GeschaeftKontakteIntern from '../containers/GeschaeftKontakteIntern'
 import GeschaeftKontakteExtern from '../containers/GeschaeftKontakteExtern'
 
-class AreaPersonen extends Component {
-  static propTypes = {
-    geschaeft: PropTypes.object,
-    interneOptions: PropTypes.array,
-    nrOfFieldsBeforePersonen: PropTypes.number,
-    change: PropTypes.func.isRequired,
-    blur: PropTypes.func.isRequired
-  }
-
-  verantwortlichData = () => {
-    const { geschaeft, interneOptions } = this.props
-    const data = interneOptions.find((o) =>
-      o.kurzzeichen === geschaeft.verantwortlich
-    )
-    if (!data) return ''
-    const name = `${data.vorname || ''} ${data.name || ''}`
-    const abt = data.abteilung ? `, ${data.abteilung}` : ''
-    const eMail = data.eMail ? `, ${data.eMail}` : ''
-    const telefon = data.telefon ? `, ${data.telefon}` : ''
-    return `${name}${abt}${eMail}${telefon}`
-  }
-
-  verwantwortlichOptions = () => {
-    const { interneOptions } = this.props
-    // sort interneOptions by kurzzeichen
-    const interneOptionsSorted = _.sortBy(interneOptions, (o) =>
-      o.kurzzeichen.toLowerCase()
-    )
-    const options = interneOptionsSorted.map((o, index) => {
-      let times = 5 - o.kurzzeichen.length
-      // make sure, times is never < 0
-      if (times < 0) {
-        times = 0
-      }
-      const space = '\xa0'.repeat(times)
-      const name = `${o.vorname || ''} ${o.name || ''}`
-      return (
-        <option key={index + 1} value={o.kurzzeichen}>
-          {`${o.kurzzeichen}${space}${'\xa0\xa0\xa0'}${name}`}
-        </option>
-      )
-    })
-    options.unshift(<option key={0} value=""></option>)
-    return options
-  }
-
-  render() {
-    const {
-      geschaeft,
-      nrOfFieldsBeforePersonen = 0,
-      change,
-      blur
-    } = this.props
-
+const verwantwortlichOptions = (interneOptions) => {
+  // sort interneOptions by kurzzeichen
+  const interneOptionsSorted = _.sortBy(interneOptions, (o) =>
+    o.kurzzeichen.toLowerCase()
+  )
+  const options = interneOptionsSorted.map((o, index) => {
+    let times = 5 - o.kurzzeichen.length
+    // make sure, times is never < 0
+    if (times < 0) {
+      times = 0
+    }
+    const space = '\xa0'.repeat(times)
+    const name = `${o.vorname || ''} ${o.name || ''}`
     return (
-      <div className={styles.areaPersonen}>
-        <div className={styles.areaPersonenTitle}>
-          Personen
-        </div>
-        <div className={styles.areaVerantwortlichSubTitle}>
-          <div className={styles.areaSubTitleDiv}>
-            Verantwortlich
-          </div>
-        </div>
-        <div className={styles.fieldVerantwortlich}>
-          <FormControl
-            componentClass="select"
-            value={geschaeft.verantwortlich || ''}
-            name="verantwortlich"
-            onChange={change}
-            onBlur={blur}
-            bsSize="small"
-            tabIndex={1 + nrOfFieldsBeforePersonen}
-            className={styles.verantwDropdown}
-          >
-            {this.verwantwortlichOptions()}
-          </FormControl>
-        </div>
-        <div className={styles.fieldVerantwortlichName}>
-          <FormControl.Static>
-            {this.verantwortlichData()}
-          </FormControl.Static>
-        </div>
-        <div className={styles.areaInterneKontakteSubTitle}>
-          <div className={styles.areaSubTitleDiv}>
-            Interne Kontakte
-          </div>
-        </div>
-        <GeschaeftKontakteIntern
-          tabIndex={nrOfFieldsBeforePersonen + 1}
-        />
-        <div className={styles.areaExterneKontakteSubTitle}>
-          <div className={styles.areaSubTitleDiv}>
-            Externe Kontakte
-          </div>
-        </div>
-        <GeschaeftKontakteExtern
-          tabIndex={nrOfFieldsBeforePersonen + 2}
-        />
-      </div>
+      <option key={index + 1} value={o.kurzzeichen}>
+        {`${o.kurzzeichen}${space}${'\xa0\xa0\xa0'}${name}`}
+      </option>
     )
-  }
+  })
+  options.unshift(<option key={0} value=""></option>)
+  return options
+}
+
+const verantwortlichData = (geschaeft, interneOptions) => {
+  const data = interneOptions.find((o) =>
+    o.kurzzeichen === geschaeft.verantwortlich
+  )
+  if (!data) return ''
+  const name = `${data.vorname || ''} ${data.name || ''}`
+  const abt = data.abteilung ? `, ${data.abteilung}` : ''
+  const eMail = data.eMail ? `, ${data.eMail}` : ''
+  const telefon = data.telefon ? `, ${data.telefon}` : ''
+  return `${name}${abt}${eMail}${telefon}`
+}
+
+const AreaPersonen = ({
+  geschaeft,
+  nrOfFieldsBeforePersonen = 0,
+  change,
+  blur,
+  interneOptions
+}) =>
+  <div className={styles.areaPersonen}>
+    <div className={styles.areaPersonenTitle}>
+      Personen
+    </div>
+    <div className={styles.areaVerantwortlichSubTitle}>
+      <div className={styles.areaSubTitleDiv}>
+        Verantwortlich
+      </div>
+    </div>
+    <div className={styles.fieldVerantwortlich}>
+      <FormControl
+        componentClass="select"
+        value={geschaeft.verantwortlich || ''}
+        name="verantwortlich"
+        onChange={change}
+        onBlur={blur}
+        bsSize="small"
+        tabIndex={1 + nrOfFieldsBeforePersonen}
+        className={styles.verantwDropdown}
+      >
+        {verwantwortlichOptions(interneOptions)}
+      </FormControl>
+    </div>
+    <div className={styles.fieldVerantwortlichName}>
+      <FormControl.Static>
+        {verantwortlichData(geschaeft, interneOptions)}
+      </FormControl.Static>
+    </div>
+    <div className={styles.areaInterneKontakteSubTitle}>
+      <div className={styles.areaSubTitleDiv}>
+        Interne Kontakte
+      </div>
+    </div>
+    <GeschaeftKontakteIntern
+      tabIndex={nrOfFieldsBeforePersonen + 1}
+    />
+    <div className={styles.areaExterneKontakteSubTitle}>
+      <div className={styles.areaSubTitleDiv}>
+        Externe Kontakte
+      </div>
+    </div>
+    <GeschaeftKontakteExtern
+      tabIndex={nrOfFieldsBeforePersonen + 2}
+    />
+  </div>
+
+AreaPersonen.displayName = 'AreaPersonen'
+
+AreaPersonen.propTypes = {
+  geschaeft: PropTypes.object,
+  interneOptions: PropTypes.array,
+  nrOfFieldsBeforePersonen: PropTypes.number,
+  change: PropTypes.func.isRequired,
+  blur: PropTypes.func.isRequired
 }
 
 export default AreaPersonen
