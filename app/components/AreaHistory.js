f77a1189815cc0b4669bcc21cd3e4318ad00a6db
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-21 10:20:13 +0200

    convert to function

--- app/components/AreaHistory.js
+++ app/components/AreaHistory.js
@@ -1,45 +1,41 @@
 'use strict'
 
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
 import { FormControl, ControlLabel } from 'react-bootstrap'
 import moment from 'moment'
 moment.locale('de')
 import styles from './AreaHistory.css'
 import AreaHistoryRows from '../containers/AreaHistoryRows'
 
-class AreaHistory extends Component {
-  static propTypes = {
-    geschaeft: PropTypes.object,
-    blur: PropTypes.func.isRequired,
-    change: PropTypes.func.isRequired
-  }
+const AreaHistory = ({ geschaeft, blur, change }) =>
+  <div className={styles.areaHistory}>
+    <div className={styles.areaHistoryTitle}>
+      Historie
+    </div>
+    <ControlLabel className={styles.labelVorgeschaeft}>
+      Vorgeschäft
+    </ControlLabel>
+    <div className={styles.fieldVorgeschaeft}>
+      <FormControl
+        type="number"
+        value={geschaeft.idVorgeschaeft || ''}
+        name="idVorgeschaeft"
+        onChange={change}
+        onBlur={blur}
+        bsSize="small"
+        placeholder="ID"
+        tabIndex={99}
+      />
+    </div>
+    <AreaHistoryRows />
+  </div>
 
-  render() {
-    const { geschaeft, blur, change } = this.props
-    return (
-      <div className={styles.areaHistory}>
-        <div className={styles.areaHistoryTitle}>
-          Historie
-        </div>
-        <ControlLabel className={styles.labelVorgeschaeft}>
-          Vorgeschäft
-        </ControlLabel>
-        <div className={styles.fieldVorgeschaeft}>
-          <FormControl
-            type="number"
-            value={geschaeft.idVorgeschaeft || ''}
-            name="idVorgeschaeft"
-            onChange={change}
-            onBlur={blur}
-            bsSize="small"
-            placeholder="ID"
-            tabIndex={99}
-          />
-        </div>
-        <AreaHistoryRows />
-      </div>
-    )
-  }
+AreaHistory.displayName = 'AreaHistory'
+
+AreaHistory.propTypes = {
+  geschaeft: PropTypes.object,
+  blur: PropTypes.func.isRequired,
+  change: PropTypes.func.isRequired
 }
 
 export default AreaHistory
