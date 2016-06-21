c5e7d5531e4d24aaef73bfa4e9cc02f4406ca1d3
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-21 11:17:45 +0200

    modularize

--- app/components/GeschaeftKontakteExternItems.js
+++ app/components/GeschaeftKontakteExternItems.js
@@ -1,100 +1,95 @@
 'use strict'
 
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
 import { Glyphicon } from 'react-bootstrap'
 import _ from 'lodash'
 import styles from './GeschaeftKontakteExtern.css'
 
-class GeschaefteKontakteExtern extends Component {
-  static propTypes = {
-    externeOptions: PropTypes.array.isRequired,
-    geschaefteKontakteExtern: PropTypes.array.isRequired,
-    geschaeftKontaktExternRemove: PropTypes.func.isRequired,
-    activeId: PropTypes.number.isRequired
+const verantwortlichData = (gkI, externeOptions) => {
+  function addValueToInfo(info, value) {
+    if (!value) return info
+    if (info) return `${info}, ${value}`
+    return value
   }
+  const data = externeOptions.find((o) =>
+    o.id === gkI.idKontakt
+  )
+  if (!data) return ''
+  let info = ''
+  info = addValueToInfo(info, data.firma)
+  info = addValueToInfo(info, data.email)
+  info = addValueToInfo(info, data.telefon)
+  return info
+}
 
-  onClickRemove = (idKontakt) => {
-    const { activeId, geschaeftKontaktExternRemove } = this.props
-    geschaeftKontaktExternRemove(activeId, idKontakt)
-  }
+const titleText = (idKontakt, externeOptions) => {
+  const data = externeOptions.find((o) =>
+    o.id === idKontakt
+  )
+  if (!data) return 'Kontakt entfernen'
+  return `${data.name} ${data.vorname} entfernen`
+}
 
-  verantwortlichData = (gkI) => {
-    function addValueToInfo(info, value) {
-      if (!value) return info
-      if (info) return `${info}, ${value}`
-      return value
-    }
-    const { externeOptions } = this.props
-    const data = externeOptions.find((o) =>
-      o.id === gkI.idKontakt
+const GeschaefteKontakteExtern = ({
+  geschaefteKontakteExtern,
+  activeId,
+  externeOptions,
+  geschaeftKontaktExternRemove
+}) => {
+  // filter for this geschaeft
+  const gkIFiltered = geschaefteKontakteExtern.filter((g) =>
+    g.idGeschaeft === activeId
+  )
+  const gKISorted = _.sortBy(gkIFiltered, (g) => {
+    const intOption = externeOptions.find((o) =>
+      o.id === g.idKontakt
     )
-    if (!data) return ''
-    let info = ''
-    info = addValueToInfo(info, data.firma)
-    info = addValueToInfo(info, data.email)
-    info = addValueToInfo(info, data.telefon)
-    return info
-  }
-
-  titleText = (idKontakt) => {
-    const { externeOptions } = this.props
-    const data = externeOptions.find((o) =>
-      o.id === idKontakt
-    )
-    if (!data) return 'Kontakt entfernen'
-    return `${data.name} ${data.vorname} entfernen`
-  }
-
-  render = () => {
-    const {
-      geschaefteKontakteExtern,
-      activeId,
-      externeOptions
-    } = this.props
-    // filter for this geschaeft
-    const gkIFiltered = geschaefteKontakteExtern.filter((g) =>
-      g.idGeschaeft === activeId
-    )
-    const gKISorted = _.sortBy(gkIFiltered, (g) => {
-      const intOption = externeOptions.find((o) =>
-        o.id === g.idKontakt
-      )
-      return `${intOption.name} ${intOption.vorname}`.toLowerCase()
-    })
-    return (
-      <div className={styles.body}>
-        {
-          gKISorted.map((gkI, index) => {
-            const intOption = externeOptions.find((o) =>
-              o.id === gkI.idKontakt
-            )
-            const nameVorname = intOption.nameVorname
-            return (
-              <div
-                key={index + 1}
-                className={styles.row}
-              >
-                <div className={styles.fV}>
-                  {nameVorname}
-                </div>
-                <div className={styles.fVN}>
-                  {this.verantwortlichData(gkI)}
-                </div>
-                <div className={styles.deleteGlyphiconDiv}>
-                  <Glyphicon
-                    glyph="remove-circle"
-                    onClick={() => this.onClickRemove(gkI.idKontakt)}
-                    className={styles.removeGlyphicon}
-                    title={this.titleText(gkI.idKontakt)}
-                  />
-                </div>
+    return `${intOption.name} ${intOption.vorname}`.toLowerCase()
+  })
+  return (
+    <div className={styles.body}>
+      {
+        gKISorted.map((gkI, index) => {
+          const intOption = externeOptions.find((o) =>
+            o.id === gkI.idKontakt
+          )
+          const nameVorname = intOption.nameVorname
+          return (
+            <div
+              key={index + 1}
+              className={styles.row}
+            >
+              <div className={styles.fV}>
+                {nameVorname}
               </div>
-            )
-          })
-        }
-      </div>
-    )
-  }
+              <div className={styles.fVN}>
+                {verantwortlichData(gkI, externeOptions)}
+              </div>
+              <div className={styles.deleteGlyphiconDiv}>
+                <Glyphicon
+                  glyph="remove-circle"
+                  onClick={() =>
+                    geschaeftKontaktExternRemove(activeId, gkI.idKontakt)
+                  }
+                  className={styles.removeGlyphicon}
+                  title={titleText(gkI.idKontakt, externeOptions)}
+                />
+              </div>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+GeschaefteKontakteExtern.displayName = 'GeschaefteKontakteExtern'
+
+GeschaefteKontakteExtern.propTypes = {
+  externeOptions: PropTypes.array.isRequired,
+  geschaefteKontakteExtern: PropTypes.array.isRequired,
+  geschaeftKontaktExternRemove: PropTypes.func.isRequired,
+  activeId: PropTypes.number.isRequired
 }
 
 export default GeschaefteKontakteExtern
--- app/components/GeschaeftKontakteIntern.js
+++ app/components/GeschaeftKontakteIntern.js
@@ -5,6 +5,77 @@ import { FormControl, Glyphicon } from 'react-bootstrap'
 import _ from 'lodash'
 import styles from './GeschaeftKontakteIntern.css'
 
+const onChangeNewKontaktIntern = (e, geschaeftKontaktInternNewCreate, activeId) => {
+  const idKontakt = e.target.value
+  geschaeftKontaktInternNewCreate(activeId, idKontakt)
+  // need to empty dropdown
+  e.target.value = ''
+}
+
+const titleText = (idKontakt, interneOptions) => {
+  const data = interneOptions.find((o) =>
+    o.id === idKontakt
+  )
+  if (!data) return 'Kontakt entfernen'
+  return `${data.kurzzeichen} entfernen`
+}
+
+const verantwortlichData = (gkI, interneOptions) => {
+  const data = interneOptions.find((o) =>
+    o.id === gkI.idKontakt
+  )
+  if (!data) return ''
+  const name = `${data.vorname} ${data.name}`
+  const abt = data.abteilung ? `, ${data.abteilung}` : ''
+  const eMail = data.eMail ? `, ${data.eMail}` : ''
+  const telefon = data.telefon ? `, ${data.telefon}` : ''
+  return `${name}${abt}${eMail}${telefon}`
+}
+
+const optionsList = (
+  interneOptions,
+  geschaefteKontakteIntern,
+  activeId
+) => {
+  // filter out options already choosen
+  const kontakteInternOfActiveGeschaeft = geschaefteKontakteIntern.filter((g) =>
+    g.idGeschaeft === activeId
+  )
+  const idKontakteOfGkiOfActiveGeschaeft = kontakteInternOfActiveGeschaeft.map((kI) =>
+    kI.idKontakt
+  )
+  const interneOptionsFiltered = interneOptions.filter((o) =>
+    !idKontakteOfGkiOfActiveGeschaeft.includes(o.id)
+  )
+  // sort interneOptions by kurzzeichen
+  const interneOptionsSorted = _.sortBy(interneOptionsFiltered, (o) =>
+    o.kurzzeichen.toLowerCase()
+  )
+  const options = interneOptionsSorted.map((o, index) => {
+    let times = 5 - o.kurzzeichen.length
+    // make sure, times is never < 0
+    if (times < 0) times = 0
+    const space = '\xa0'.repeat(times)
+    const name = `${o.vorname || ''} ${o.name || ''}`
+    return (
+      <option
+        key={index + 1}
+        value={o.id}
+      >
+        {`${o.kurzzeichen}${space}${'\xa0\xa0\xa0'}${name}`}
+      </option>
+    )
+  })
+  options.unshift(
+    <option
+      key={0}
+      value=""
+    >
+    </option>
+  )
+  return options
+}
+
 class GeschaefteKontakteIntern extends Component {
   static propTypes = {
     interneOptions: PropTypes.array.isRequired,
@@ -17,94 +88,12 @@ class GeschaefteKontakteIntern extends Component {
     tabIndex: PropTypes.number.isRequired
   }
 
-  onChangeNewKontaktIntern = (e) => {
-    const {
-      geschaeftKontaktInternNewCreate,
-      activeId
-    } = this.props
-    const idKontakt = e.target.value
-    geschaeftKontaktInternNewCreate(activeId, idKontakt)
-    // need to empty dropdown
-    e.target.value = ''
-  }
-
-  onClickRemove = (idKontakt) => {
-    const { activeId, geschaeftKontaktInternRemove } = this.props
-    geschaeftKontaktInternRemove(activeId, idKontakt)
-  }
-
-  options = () => {
-    const {
-      interneOptions,
-      geschaefteKontakteIntern,
-      activeId
-    } = this.props
-    // filter out options already choosen
-    const kontakteInternOfActiveGeschaeft = geschaefteKontakteIntern.filter((g) =>
-      g.idGeschaeft === activeId
-    )
-    const idKontakteOfGkiOfActiveGeschaeft = kontakteInternOfActiveGeschaeft.map((kI) =>
-      kI.idKontakt
-    )
-    const interneOptionsFiltered = interneOptions.filter((o) =>
-      !idKontakteOfGkiOfActiveGeschaeft.includes(o.id)
-    )
-    // sort interneOptions by kurzzeichen
-    const interneOptionsSorted = _.sortBy(interneOptionsFiltered, (o) =>
-      o.kurzzeichen.toLowerCase()
-    )
-    const options = interneOptionsSorted.map((o, index) => {
-      let times = 5 - o.kurzzeichen.length
-      // make sure, times is never < 0
-      if (times < 0) times = 0
-      const space = '\xa0'.repeat(times)
-      const name = `${o.vorname || ''} ${o.name || ''}`
-      return (
-        <option
-          key={index + 1}
-          value={o.id}
-        >
-          {`${o.kurzzeichen}${space}${'\xa0\xa0\xa0'}${name}`}
-        </option>
-      )
-    })
-    options.unshift(
-      <option
-        key={0}
-        value=""
-      >
-      </option>
-    )
-    return options
-  }
-
-  verantwortlichData = (gkI) => {
-    const { interneOptions } = this.props
-    const data = interneOptions.find((o) =>
-      o.id === gkI.idKontakt
-    )
-    if (!data) return ''
-    const name = `${data.vorname} ${data.name}`
-    const abt = data.abteilung ? `, ${data.abteilung}` : ''
-    const eMail = data.eMail ? `, ${data.eMail}` : ''
-    const telefon = data.telefon ? `, ${data.telefon}` : ''
-    return `${name}${abt}${eMail}${telefon}`
-  }
-
-  titleText = (idKontakt) => {
-    const { interneOptions } = this.props
-    const data = interneOptions.find((o) =>
-      o.id === idKontakt
-    )
-    if (!data) return 'Kontakt entfernen'
-    return `${data.kurzzeichen} entfernen`
-  }
-
   renderItems() {
     const {
       geschaefteKontakteIntern,
       activeId,
-      interneOptions
+      interneOptions,
+      geschaeftKontaktInternRemove
     } = this.props
     // filter for this geschaeft
     const gkIFiltered = geschaefteKontakteIntern.filter((g) =>
@@ -130,14 +119,14 @@ class GeschaefteKontakteIntern extends Component {
             {kurzzeichen}
           </div>
           <div className={styles.fVN}>
-              {this.verantwortlichData(gkI)}
+              {verantwortlichData(gkI, interneOptions)}
           </div>
           <div className={styles.deleteGlyphiconDiv}>
             <Glyphicon
               glyph="remove-circle"
-              onClick={() => this.onClickRemove(gkI.idKontakt)}
+              onClick={() => geschaeftKontaktInternRemove(activeId, gkI.idKontakt)}
               className={styles.removeGlyphicon}
-              title={this.titleText(gkI.idKontakt)}
+              title={titleText(gkI.idKontakt, interneOptions)}
             />
           </div>
         </div>
@@ -146,7 +135,13 @@ class GeschaefteKontakteIntern extends Component {
   }
 
   render = () => {
-    const { tabIndex } = this.props
+    const {
+      tabIndex,
+      geschaeftKontaktInternNewCreate,
+      activeId,
+      interneOptions,
+      geschaefteKontakteIntern
+    } = this.props
     return (
       <div className={styles.body}>
         {this.renderItems()}
@@ -159,11 +154,19 @@ class GeschaefteKontakteIntern extends Component {
               componentClass="select"
               bsSize="small"
               className={styles.dropdown}
-              onChange={this.onChangeNewKontaktIntern}
+              onChange={(e) =>
+                onChangeNewKontaktIntern(e, geschaeftKontaktInternNewCreate, activeId)
+              }
               title="Neuen Kontakt hinzufügen"
               tabIndex={tabIndex}
             >
-              {this.options()}
+              {
+                optionsList(
+                  interneOptions,
+                  geschaefteKontakteIntern,
+                  activeId
+                )
+              }
             </FormControl>
           </div>
         </div>
