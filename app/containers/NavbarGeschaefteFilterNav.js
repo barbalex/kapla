8cd036220c0224d12e67eb2a9cdd5a8b3cc74f35
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-21 18:46:19 +0200

    work on modularizing

---
+++ app/containers/NavbarGeschaefteFilterNav.js
@@ -0,0 +1,63 @@
+'use strict'
+
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import NavbarBerichteNav from '../components/NavbarBerichteNav'
+import * as AppActions from '../actions/app'
+import * as GeschaefteActions from '../actions/geschaefte'
+import * as TableActions from '../actions/table'
+import * as PagesActions from '../actions/pages'
+
+const actions = Object.assign(
+  AppActions,
+  GeschaefteActions,
+  TableActions,
+  PagesActions
+)
+
+function mapStateToProps(state) {
+  const {
+    activeId,
+    filterFulltext,
+    filterType,
+    geschaefte,
+    geschaefteGefilterteIds,
+    willDelete
+  } = state.geschaefte
+  const { username } = state.user
+  const {
+    dbPath,
+    db,
+    showMessageModal
+  } = state.app
+  const {
+    table,
+    rows,
+    id
+  } = state.table
+  const { pages } = state
+  const path = state.routing.locationBeforeTransitions.pathname
+  return {
+    username,
+    activeId,
+    filterFulltext,
+    filterType,
+    geschaefte,
+    geschaefteGefilterteIds,
+    dbPath,
+    db,
+    willDeleteGeschaeft: willDelete,
+    path,
+    showMessageModal,
+    table,
+    rows,
+    activeTableRowId: id,
+    pages
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(actions, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavbarBerichteNav)
