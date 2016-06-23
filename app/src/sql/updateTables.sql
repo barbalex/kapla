ef0e10cadb76781c5497d9e75d9f8ab440316066
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-23 15:37:56 +0200

    finish enforcing references

--- app/src/sql/updateTables.sql
+++ app/src/sql/updateTables.sql
@@ -20,8 +20,24 @@ INSERT INTO geschaefte2 (abteilung, aktennummer, aktenstandort, ausloeser, benut
 SELECT abteilung, aktennummer, aktenstandort, ausloeser, benutzer, datumAusgangAwel, datumEingangAwel, details, entscheidAwelJahr, entscheidAwelNr, entscheidBdvJahr, entscheidBdvNr, entscheidBvvJahr, entscheidBvvNr, entscheidKrJahr, entscheidKrNr, entscheidRrbJahr, entscheidRrbNr, fristAbteilung, fristAmtschef, fristAwel, fristDirektion, fristMitarbeiter, gegenstand, gekoNr, geschaeftsart, idGeschaeft, idKontaktExtern_readonly, idKontaktIntern_readonly, idVorgeschaeft, mutationsdatum, mutationsperson, naechsterSchritt, ort, parlVorstossStufe, parlVorstossTyp, rechtsmittelInstanz, rechtsmittelErledigung, rechtsmittelEntscheidNr, rechtsmittelEntscheidDatum, status, verantwortlich, vermerk, vermerkIntern, zustaendigeDirektion
 FROM geschaefte
 
-drop table geschaefte
+DROP TABLE geschaefte
 
-alter table geschaefte2 rename to geschaefte
+ALTER TABLE geschaefte2 RENAME TO geschaefte
 
 
+--geschaefteKontakteIntern
+INSERT INTO
+  geschaefteKontakteIntern2(idGeschaeft, idKontakt)
+SELECT
+  geschaefteKontakteIntern.idGeschaeft, geschaefteKontakteIntern.idKontakt
+FROM
+  geschaefteKontakteIntern
+LEFT JOIN
+    geschaefte
+    ON geschaefte.idGeschaeft = geschaefteKontakteIntern.idGeschaeft
+WHERE
+  geschaefte.idGeschaeft IS NOT NULL;
+
+DROP TABLE geschaefteKontakteIntern
+
+ALTER TABLE geschaefteKontakteIntern2 RENAME TO geschaefteKontakteIntern
