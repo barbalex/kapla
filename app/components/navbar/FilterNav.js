8564d17ab0dd3e6924720f33024005035349c86f
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-27 09:54:39 +0200

    convert to function

--- app/components/navbar/FilterNav.js
+++ app/components/navbar/FilterNav.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
 import { withRouter } from 'react-router'
 import {
   MenuItem,
@@ -53,170 +53,168 @@ const onSelectFilterVernehmlLaeuft = (geschaefteFilterByFields) => {
   geschaefteFilterByFields(filter, 'laufende Vernehmlassungen')
 }
 
-class NavbarComponent extends Component {
-  static propTypes = {
-    filterType: PropTypes.string,
-    geschaefte: PropTypes.array.isRequired,
-    geschaefteGefilterteIds: PropTypes.array.isRequired,
-    geschaefteFilterByFields: PropTypes.func.isRequired,
-    geschaefteFilterByFulltext: PropTypes.func.isRequired,
-    geschaefteRemoveFilters: PropTypes.func.isRequired,
-    filterFulltext: PropTypes.string,
-    username: PropTypes.string,
-    router: PropTypes.shape({
-      push: PropTypes.func.isRequired
-    }).isRequired,
-    path: PropTypes.string.isRequired
-  }
-
-  render() {
-    const {
-      filterFulltext,
-      filterType,
-      geschaefte,
-      geschaefteGefilterteIds,
-      geschaefteFilterByFields,
-      username,
-      geschaefteRemoveFilters,
-      geschaefteFilterByFulltext,
-      path,
-      router
-    } = this.props
-    const dataIsFilteredByFulltext = (
-      geschaefte.length !== geschaefteGefilterteIds.length &&
-      filterFulltext
-    )
-    const dataIsFilteredByFields = (
-      geschaefte.length !== geschaefteGefilterteIds.length &&
-      !filterFulltext
-    )
-    const dataIsFiltered = (
-      geschaefte.length !== geschaefteGefilterteIds.length
-    )
-    const dataIsFilteredByFulltextStyle = [
-      styles.filterInput,
-      styles.filterInputActive
-    ].join(' ')
-    const classNameFilterInput = (
-      dataIsFilteredByFulltext ?
-      dataIsFilteredByFulltextStyle :
-      styles.filterInput
-    )
-    const activeFiltercriteria = (
-      dataIsFilteredByFields ?
-      'TODO' :
-      '(kein Filter aktiv)'
-    )
-    return (
-      <Navbar.Form
-        pullLeft
-        className={styles.filterGroupContainer}
-      >
-        <div className={styles.filterGroup}>
-          <FormControl
-            type="text"
-            placeholder="Volltext filtern"
-            value={filterFulltext}
-            onChange={(e) =>
-              geschaefteFilterByFulltext(e.target.value)
+const FilterNav = ({
+  filterFulltext,
+  filterType,
+  geschaefte,
+  geschaefteGefilterteIds,
+  geschaefteFilterByFields,
+  username,
+  geschaefteRemoveFilters,
+  geschaefteFilterByFulltext,
+  path,
+  router
+}) => {
+  const dataIsFilteredByFulltext = (
+    geschaefte.length !== geschaefteGefilterteIds.length &&
+    filterFulltext
+  )
+  const dataIsFilteredByFields = (
+    geschaefte.length !== geschaefteGefilterteIds.length &&
+    !filterFulltext
+  )
+  const dataIsFiltered = (
+    geschaefte.length !== geschaefteGefilterteIds.length
+  )
+  const dataIsFilteredByFulltextStyle = [
+    styles.filterInput,
+    styles.filterInputActive
+  ].join(' ')
+  const classNameFilterInput = (
+    dataIsFilteredByFulltext ?
+    dataIsFilteredByFulltextStyle :
+    styles.filterInput
+  )
+  const activeFiltercriteria = (
+    dataIsFilteredByFields ?
+    'TODO' :
+    '(kein Filter aktiv)'
+  )
+  return (
+    <Navbar.Form
+      pullLeft
+      className={styles.filterGroupContainer}
+    >
+      <div className={styles.filterGroup}>
+        <FormControl
+          type="text"
+          placeholder="Volltext filtern"
+          value={filterFulltext}
+          onChange={(e) =>
+            geschaefteFilterByFulltext(e.target.value)
+          }
+          className={classNameFilterInput}
+          title="Zum Filtern drücken Sie die Enter-Taste"
+        />
+        <SplitButton
+          id="field-filter-dropdown"
+          title="Felder filtern"
+          className={styles.fieldFilterDropdown}
+          style={{ backgroundColor: dataIsFilteredByFields ? '#FFBF73' : null }}
+          onClick={() => {
+            if (path !== '/filterFields') {
+              router.push('/filterFields')
             }
-            className={classNameFilterInput}
-            title="Zum Filtern drücken Sie die Enter-Taste"
-          />
-          <SplitButton
-            id="field-filter-dropdown"
-            title="Felder filtern"
-            className={styles.fieldFilterDropdown}
-            style={{ backgroundColor: dataIsFilteredByFields ? '#FFBF73' : null }}
-            onClick={() => {
-              if (path !== '/filterFields') {
-                geschaefteRemoveFilters()
-                router.push('/filterFields')
-              }
+          }}
+        >
+          <MenuItem header>
+            aktive Filterkriterien:
+          </MenuItem>
+          <MenuItem header>
+            {activeFiltercriteria}
+          </MenuItem>
+          <MenuItem header>
+            vorbereitete Filter:
+          </MenuItem>
+          <MenuItem
+            onSelect={() =>
+              onSelectFilterFaelligeGeschaefte(geschaefteFilterByFields)
+            }
+            style={{
+              backgroundColor: (
+                filterType === 'fällige' ?
+                '#FFBF73' :
+                null
+              )
             }}
           >
-            <MenuItem header>
-              aktive Filterkriterien:
-            </MenuItem>
-            <MenuItem header>
-              {activeFiltercriteria}
-            </MenuItem>
-            <MenuItem header>
-              vorbereitete Filter:
-            </MenuItem>
-            <MenuItem
-              onSelect={() =>
-                onSelectFilterFaelligeGeschaefte(geschaefteFilterByFields)
-              }
-              style={{
-                backgroundColor: (
-                  filterType === 'fällige' ?
-                  '#FFBF73' :
-                  null
-                )
-              }}
-            >
-              fällige Geschäfte
-            </MenuItem>
-            <MenuItem
-              onSelect={() =>
-                onSelectFilterFaelligeGeschaefteMitarbeiter(geschaefteFilterByFields, username)
-              }
-              style={{
-                backgroundColor: (
-                  filterType === 'eigene fällige' ?
-                  '#FFBF73' :
-                  null
-                )
-              }}
-            >
-              eigene fällige Geschäfte
-            </MenuItem>
-            <MenuItem
-              onSelect={() =>
-                onSelectFilterVernehmlAngek(geschaefteFilterByFields)
-              }
-              style={{
-                backgroundColor: (
-                  filterType === 'angekündigte Vernehmlassungen' ?
-                  '#FFBF73' :
-                  null
-                )
-              }}
-            >
-              angekündigte Vernehmlassungen
-            </MenuItem>
-            <MenuItem
-              onSelect={() =>
-                onSelectFilterVernehmlLaeuft(geschaefteFilterByFields)
-              }
-              style={{
-                backgroundColor: (
-                  filterType === 'laufende Vernehmlassungen' ?
-                  '#FFBF73' :
-                  null
-                )
-              }}
-            >
-              laufende Vernehmlassungen
-            </MenuItem>
-          </SplitButton>
-          <Button
-            disabled={!dataIsFiltered}
-            className={styles.filterRemoveButton}
+            fällige Geschäfte
+          </MenuItem>
+          <MenuItem
+            onSelect={() =>
+              onSelectFilterFaelligeGeschaefteMitarbeiter(geschaefteFilterByFields, username)
+            }
+            style={{
+              backgroundColor: (
+                filterType === 'eigene fällige' ?
+                '#FFBF73' :
+                null
+              )
+            }}
           >
-            <Glyphicon
-              glyph="remove"
-              onClick={() =>
-                geschaefteRemoveFilters()
-              }
-              title="Filter entfernen"
-            />
-          </Button>
-        </div>
-      </Navbar.Form>
-    )
-  }
+            eigene fällige Geschäfte
+          </MenuItem>
+          <MenuItem
+            onSelect={() =>
+              onSelectFilterVernehmlAngek(geschaefteFilterByFields)
+            }
+            style={{
+              backgroundColor: (
+                filterType === 'angekündigte Vernehmlassungen' ?
+                '#FFBF73' :
+                null
+              )
+            }}
+          >
+            angekündigte Vernehmlassungen
+          </MenuItem>
+          <MenuItem
+            onSelect={() =>
+              onSelectFilterVernehmlLaeuft(geschaefteFilterByFields)
+            }
+            style={{
+              backgroundColor: (
+                filterType === 'laufende Vernehmlassungen' ?
+                '#FFBF73' :
+                null
+              )
+            }}
+          >
+            laufende Vernehmlassungen
+          </MenuItem>
+        </SplitButton>
+        <Button
+          disabled={!dataIsFiltered}
+          className={styles.filterRemoveButton}
+        >
+          <Glyphicon
+            glyph="remove"
+            onClick={() =>
+              geschaefteRemoveFilters()
+            }
+            title="Filter entfernen"
+          />
+        </Button>
+      </div>
+    </Navbar.Form>
+  )
 }
 
-export default withRouter(NavbarComponent)
+FilterNav.displayName = 'FilterNav'
+
+FilterNav.propTypes = {
+  filterType: PropTypes.string,
+  geschaefte: PropTypes.array.isRequired,
+  geschaefteGefilterteIds: PropTypes.array.isRequired,
+  geschaefteFilterByFields: PropTypes.func.isRequired,
+  geschaefteFilterByFulltext: PropTypes.func.isRequired,
+  geschaefteRemoveFilters: PropTypes.func.isRequired,
+  filterFulltext: PropTypes.string,
+  username: PropTypes.string,
+  router: PropTypes.shape({
+    push: PropTypes.func.isRequired
+  }).isRequired,
+  path: PropTypes.string.isRequired
+}
+
+export default withRouter(FilterNav)
