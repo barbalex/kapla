dd5088e87a09931763fae207f630b4accd3c6cd4
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-30 09:22:12 +0200

    convert to function

--- app/components/ModalGeschaeftDelete.js
+++ app/components/ModalGeschaeftDelete.js
@@ -1,50 +1,45 @@
 'use strict'
 
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
 import { Modal, Button } from 'react-bootstrap'
 
-class ModalGeschaeftDelete extends Component {
-  static propTypes = {
-    geschaeftRemove: PropTypes.func.isRequired,
-    geschaeftRemoveDeleteIntended: PropTypes.func.isRequired,
-    activeId: PropTypes.number,
-  }
+const ModalGeschaeftDelete = ({
+  geschaeftRemove,
+  geschaeftRemoveDeleteIntended,
+  activeId,
+}) =>
+  <Modal.Dialog>
+    <Modal.Header>
+      <Modal.Title>
+        Geschäft löschen
+      </Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      Möchten Sie das Geschäft Nr. {activeId} wirklich löschen?
+    </Modal.Body>
+    <Modal.Footer>
+      <Button
+        onClick={geschaeftRemoveDeleteIntended}
+      >
+        Nein
+      </Button>
+      <Button
+        bsStyle="primary"
+        onClick={() =>
+          geschaeftRemove(activeId)
+        }
+      >
+        Ja
+      </Button>
+    </Modal.Footer>
+  </Modal.Dialog>
 
-  render() {
-    const {
-      geschaeftRemove,
-      geschaeftRemoveDeleteIntended,
-      activeId,
-    } = this.props
+ModalGeschaeftDelete.displayName = 'ModalGeschaeftDelete'
 
-    return (
-      <Modal.Dialog>
-        <Modal.Header>
-          <Modal.Title>
-            Geschäft löschen
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          Möchten Sie das Geschäft Nr. {activeId} wirklich löschen?
-        </Modal.Body>
-        <Modal.Footer>
-          <Button
-            onClick={geschaeftRemoveDeleteIntended}
-          >
-            Nein
-          </Button>
-          <Button
-            bsStyle="primary"
-            onClick={() =>
-              geschaeftRemove(activeId)
-            }
-          >
-            Ja
-          </Button>
-        </Modal.Footer>
-      </Modal.Dialog>
-    )
-  }
+ModalGeschaeftDelete.propTypes = {
+  geschaeftRemove: PropTypes.func.isRequired,
+  geschaeftRemoveDeleteIntended: PropTypes.func.isRequired,
+  activeId: PropTypes.number,
 }
 
 export default ModalGeschaeftDelete
