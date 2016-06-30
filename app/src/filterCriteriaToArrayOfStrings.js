34926f6e17cca06039dcee5edfcf01ba72fef099
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-30 18:04:38 +0200

    show only criteria with values

--- app/src/filterCriteriaToArrayOfStrings.js
+++ app/src/filterCriteriaToArrayOfStrings.js
@@ -1,5 +1,8 @@
 export default (filterFields) =>
   filterFields
+    .filter((ff) =>
+      ff.value || ff.value === 0
+    )
     .map((ff) =>
       `${ff.field} ${ff.comparator} '${ff.value}'`
     )
