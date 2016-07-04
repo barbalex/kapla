f09af0499301888f7ab97ae6a735fec0359cf663
Alexander Gabriel <alex@gabriel-software.ch>
2016-07-04 15:54:35 +0200

    keep filter when sorting

--- app/reducers/geschaefte/geschaefteSortByFieldsGetGeschaefte.js
+++ app/reducers/geschaefte/geschaefteSortByFieldsGetGeschaefte.js
@@ -1,10 +1,17 @@
 import _ from 'lodash'
+import moment from 'moment'
+import isDateField from '../../src/isDateField'
 
 export default (state, sortFields) => {
   let { geschaefte } = state
+  const { geschaefteGefilterteIds } = state
   sortFields.forEach((sf) => {
     geschaefte = _.sortBy(geschaefte, (g) => {
       if (g[sf.field]) {
+        if (isDateField(sf.field)) {
+          // need to reformat date
+          return moment(g[sf.field], 'DD.MM.YYYY').format('YYYY-MM-DD')
+        }
         return g[sf.field]
       }
       return 'ZZZZ'
@@ -16,5 +23,7 @@ export default (state, sortFields) => {
   if (sortFields.length === 0) {
     geschaefte = _.sortBy(geschaefte, 'idGeschaeft').reverse()
   }
+  // reduce to filtered
+  geschaefte = geschaefte.filter((g) => geschaefteGefilterteIds.includes(g.idGeschaeft))
   return geschaefte
 }
