4ee323c55efa900476c4560a663ec83ca412031d
Alexander Gabriel <alex@gabriel-software.ch>
2016-10-02 22:19:28 +0200

    work more on trying to get exceljs to work

--- app/src/writeExport.js
+++ app/src/writeExport.js
@@ -2,43 +2,43 @@
  * writes a dataArray to an Excel workbook
  */
 
-import Promise from 'bluebird'
 import Excel from 'exceljs'
 
-export default (path, dataArray) =>
-  new Promise((resolve, reject) => {
-    const workbook = new Excel.Workbook()
-    const worksheet = workbook.addWorksheet(
-      'Geschäfte',
-      {
-        views: [
-          {
-            state: 'frozen',
-            xSplit: 0,
-            ySplit: 1
-          }
-        ]
-      }
-    )
-    worksheet.addRows(dataArray)
-    worksheet.getRow(1).fill = {
-      type: 'gradient',
-      gradient: 'angle',
-      degree: 0,
-      stops: [
-        { position: 0, color: { argb: 'FFD3D3D3' } },
-        { position: 1, color: { argb: 'FFD3D3D3' } }
+export default (path, dataArray) => {
+  const workbook = new Excel.Workbook()
+  const worksheet = workbook.addWorksheet(
+    'Geschäfte',
+    {
+      views: [
+        {
+          state: 'frozen',
+          xSplit: 0,
+          ySplit: 1
+        }
       ]
     }
-    worksheet.getRow(1).font = {
-      bold: true
+  )
+  worksheet.addRows(dataArray)
+  worksheet.getRow(1).fill = {
+    type: 'gradient',
+    gradient: 'angle',
+    degree: 0,
+    stops: [
+      { position: 0, color: { argb: 'FFD3D3D3' } },
+      { position: 1, color: { argb: 'FFD3D3D3' } }
+    ]
+  }
+  worksheet.getRow(1).font = {
+    bold: true
+  }
+  worksheet.getRow(1).border = {
+    bottom: {
+      style: 'thin'
     }
-    worksheet.getRow(1).border = {
-      bottom: {
-        style: 'thin'
-      }
-    }
-    workbook.xlsx.writeFile(path)
-      .then(() => resolve())
-      .catch(err => reject(err))
-  })
+  }
+  return workbook.xlsx.writeFile(path)
+    .then(() => null)
+    .catch((err) => {
+      throw err
+    })
+}
--- main.js
+++ main.js
@@ -118,7 +118,7 @@ app.on('ready', () => {
 
   // meanwhile: always show dev tools
   // if (process.env.NODE_ENV === 'development') {
-  // mainWindow.openDevTools()
+  mainWindow.openDevTools()
   // }
 
   // save window state on close
